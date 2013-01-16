1a79fb1d11823f0958cb8384034465d34a4c069f
Kaoru Maeda <kaoru.maeda@gmail.com>
2013-01-16 17:12:07 +0900

    Display settings

--- .emacs.d/conf/fonts.el
+++ .emacs.d/conf/fonts.el
@@ -1,7 +1,8 @@
 ;; -*- emacs-lisp -*-
 
 (cond ((eq (window-system) 'x)
-       (add-to-list 'default-frame-alist '(font . "ricty-12:weight=normal:spacing=0")))
+       (add-to-list 'default-frame-alist '(font . "ricty-14:weight=normal:spacing=0"))
+       )
       ((eq (window-system) 'ns)
        (set-face-attribute 'default nil :family "Ricty" :height 160)
        (set-fontset-font nil 'japanese-jisx0208        '("Ricty" . "iso10646-*"))
--- .emacs.d/conf/macosx.el
+++ .emacs.d/conf/macosx.el
@@ -1,10 +1,12 @@
-;; MacUIM
-;(setq default-input-method "MacOSX")
-;(mac-add-key-passed-to-system '(shift ? ))
+(cond ((eq window-system 'macos)
+       ;; MacUIM
+       ;;(setq default-input-method "MacOSX")
+       ;;(mac-add-key-passed-to-system '(shift ? ))
 
-;; Command-Key and Option-Key
-(setq ns-command-modifier 'meta)
-(setq ns-alternate-modifier 'super)
+       ;; Command-Key and Option-Key
+       (setq ns-command-modifier 'meta)
+       (setq ns-alternate-modifier 'super)
 
-;; frame size
-(modify-frame-parameters (selected-frame) '((width . 96) (height . 40)))
+       ;; frame size
+       (modify-frame-parameters (selected-frame) '((width . 96) (height . 40)))
+       ))
