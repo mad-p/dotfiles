88ddfdf3fa4ff919a206cb445df0c4a5c93449a3
Kaoru Maeda <kaoru.maeda@gmail.com>
2013-04-10 09:44:05 +0900

    emacs: okra setting

--- .emacs.d/local/okra.localdomain.el
+++ .emacs.d/local/okra.localdomain.el
@@ -1,2 +1,3 @@
-(if (eq (window-system) 'x)
-    (setcdr (assoc 'font default-frame-alist) "-shinonome-gothic-medium-r-normal--16-*-*-*-*-*-*-*"))
+;(if (eq (window-system) 'x)
+;    (setcdr (assoc 'font default-frame-alist) "-jis-fixed-medium-r-normal--16-*-*-*-*-*-*-*"))
+(modify-frame-parameters (selected-frame) '((width . 120) (height . 50)))
