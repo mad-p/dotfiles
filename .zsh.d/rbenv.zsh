84c60dfde31abc6637d2085503b6a08ce3de8e0d
Kaoru Maeda <kaoru.maeda@gmail.com>
2014-02-04 14:45:28 +0900

    PATH rbenv itself

--- .zsh.d/rbenv.zsh
+++ .zsh.d/rbenv.zsh
@@ -2,6 +2,6 @@ if [ -d "$HOME/.rbenv/shims" ]; then
   export PATH=$HOME/.rbenv/shims:$PATH
   eval "$(rbenv init - zsh)"
 elif [ -d "/usr/local/rbenv/shims" ]; then
-  export PATH=/usr/local/rbenv/shims:$PATH
+  export PATH=/usr/local/rbenv/bin:/usr/local/rbenv/shims:$PATH
   eval "$(rbenv init - zsh)"
 fi
