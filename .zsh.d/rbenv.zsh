98e8d42a0688dab12e08b353d0ce221ab20cd162
Kaoru Maeda <kaoru.maeda@gmail.com>
2014-02-04 13:43:48 +0900

    rbenv may reside in /usr/local

--- .zsh.d/rbenv.zsh
+++ .zsh.d/rbenv.zsh
@@ -1,4 +1,7 @@
 if [ -d "$HOME/.rbenv/shims" ]; then
   export PATH=$HOME/.rbenv/shims:$PATH
   eval "$(rbenv init - zsh)"
+elif [ -d "/usr/local/rbenv/shims" ]; then
+  export PATH=/usr/local/rbenv/shims:$PATH
+  eval "$(rbenv init - zsh)"
 fi
