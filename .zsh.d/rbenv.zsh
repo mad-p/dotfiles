b0cc3cf2953d6998a8cc87e309a9138902eb921e
Kaoru Maeda <kaoru.maeda@gmail.com>
2014-02-05 11:24:30 +0900

    Run rbenv iff it exists

--- .zsh.d/rbenv.zsh
+++ .zsh.d/rbenv.zsh
@@ -1,7 +1,9 @@
+if [ -d "/usr/local/rbenv/shims" ]; then
+  export PATH=/usr/local/rbenv/bin:/usr/local/rbenv/shims:$PATH
+fi
 if [ -d "$HOME/.rbenv/shims" ]; then
   export PATH=$HOME/.rbenv/shims:$PATH
-  eval "$(rbenv init - zsh)"
-elif [ -d "/usr/local/rbenv/shims" ]; then
-  export PATH=/usr/local/rbenv/bin:/usr/local/rbenv/shims:$PATH
+fi
+if [ -x "`which rbenv 2>/dev/null`" ]; then
   eval "$(rbenv init - zsh)"
 fi
