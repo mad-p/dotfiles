54eecfcdef6ece7c78040b0281c088cbb7767d83
Kaoru Maeda <maeda@lepidum.co.jp>
2014-05-19 15:10:33 +0900

    tsudaring support

--- .emacs.d/conf/twittering.el
+++ .emacs.d/conf/twittering.el
@@ -3,3 +3,16 @@
 %FILL[  ]{%T // %r%R}
 ")
 
+(defvar tsuda-hashtag "#idcon")
+
+(defun tsuda (beg end)
+  (interactive "r")
+  (let ((string (buffer-substring beg end))
+        (sep " "))
+    (setq string (replace-regexp-in-string "^\\s-*\\(-+\\)\\s-+" "\\1" string))
+    (if (string-match "^-" string)
+        (setq sep "\n")
+      (setq string (replace-regexp-in-string "\n" " " string)))
+    (twittering-update-status (concat string sep tsuda-hashtag))))
+
+(define-key text-mode-map "\C-c\C-c" 'tsuda)
