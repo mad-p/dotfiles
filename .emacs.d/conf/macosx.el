7a27121c28a4d4666ec9830c2d88e85f389580c8
Kaoru Maeda <kaoru.maeda@gmail.com>
2016-05-31 15:37:50 +0900

    fix macosx.el syntax error

--- .emacs.d/conf/macosx.el
+++ .emacs.d/conf/macosx.el
@@ -12,13 +12,13 @@
        (custom-set-variables
         '(mouse-wheel-progressive-speed nil)
         '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control))))))
-       )
 
-      ;; turn-around visible-bell glitches
-      (setq visible-bell nil)
-      (setq ring-bell-function (lambda ()
-                                 (invert-face 'mode-line)
-                                 (run-with-timer 0.1 nil 'invert-face 'mode-line)))
+       ;; turn-around visible-bell glitches
+       (setq visible-bell nil)
+       (setq ring-bell-function (lambda ()
+                                  (invert-face 'mode-line)
+                                  (run-with-timer 0.1 nil 'invert-face 'mode-line)))
+       )
 
       ((eq window-system 'x)
        (modify-frame-parameters (selected-frame) '((width . 96) (height . 36)))
