5c87bd014356a5df51b65ff10ae5127a2cc4c8da
Kaoru Maeda <kaoru.maeda@gmail.com>
2017-02-23 15:15:40 +0900

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,7 +31,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(calendar-date-display-form (quote (year "/" month "/" day)))
- '(calendar-time-display-form (quote (24-hours ":" minutes (if time-zone " (") time-zone (if time-zone ")"))) t)
+ '(calendar-time-display-form
+   (quote
+    (24-hours ":" minutes
+              (if time-zone " (")
+              time-zone
+              (if time-zone ")"))) t)
  '(cperl-comment-column 40)
  '(cperl-continued-statement-offset 4)
  '(cperl-indent-level 4)
@@ -40,7 +45,13 @@
  '(desktop-save-mode t)
  '(diff-switches "-u")
  '(display-time-mode t)
- '(edit-server-new-frame-alist (quote ((name . "Edit with Emacs FRAME") (width . 96) (height . 32) (minibuffer . t) (menu-bar-lines . t))))
+ '(edit-server-new-frame-alist
+   (quote
+    ((name . "Edit with Emacs FRAME")
+     (width . 96)
+     (height . 32)
+     (minibuffer . t)
+     (menu-bar-lines . t))))
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(js2-auto-indent-flag t)
@@ -59,7 +70,16 @@
  '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control)))))
  '(recentf-max-saved-items 50)
  '(recentf-mode t)
- '(safe-local-variable-values (quote ((charset . utf-8) (encoding . utf-8))))
+ '(safe-local-variable-values
+   (quote
+    ((eval highlight-regexp "^[^	]+	[^	]+	[^	]+$\\|^[^	]+	[^	]+$"
+           (quote hi-pink))
+     (eval highlight-regexp "^[^	]+	[^	]+	[^	]+$\\|^[^	]+	[^	]+$"
+           (quote hi-yellow))
+     (eval local-set-key "	"
+           (quote self-insert-command))
+     (charset . utf-8)
+     (encoding . utf-8))))
  '(sh-indentation 2)
  '(show-paren-mode t)
  '(tempbuf-minimum-timeout 60)
