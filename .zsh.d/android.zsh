2ccd76f539f1ec0d76b9e814d046ff6004dbfd3a
Kaoru Maeda <kaoru.maeda@dena.com>
2022-07-28 11:12:07 +0900

    android path

--- .zsh.d/android.zsh
+++ .zsh.d/android.zsh
@@ -1,4 +1,5 @@
 export ANDROID_HOME=/Users/kaoru.maeda/Library/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
 export NDK_ROOT=$HOME/Library/Android/sdk/ndk-bundle
 export ANDROID_NDK_ROOT=$NDK_ROOT
-export PATH=$ANDROID_NDK_ROOT:$PATH:$ANDROID_HOME/platform-tools
+export PATH=$ANDROID_NDK_ROOT:$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator
