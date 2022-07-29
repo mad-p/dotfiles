ae1bf2a1c4db54beb7de1da3e497926f3222704d
Kaoru Maeda <kaoru.maeda@dena.com>
2022-07-29 16:05:07 +0900

    Revert "android path"
    This reverts commit 51551b3f26e417621f5604963af257ea916995d7.

--- .zsh.d/android.zsh
+++ .zsh.d/android.zsh
@@ -1,5 +1,4 @@
 export ANDROID_HOME=/Users/kaoru.maeda/Library/Android/sdk
-export ANDROID_SDK_ROOT=$ANDROID_HOME
 export NDK_ROOT=$HOME/Library/Android/sdk/ndk-bundle
 export ANDROID_NDK_ROOT=$NDK_ROOT
-export PATH=$ANDROID_NDK_ROOT:$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator
+export PATH=$ANDROID_NDK_ROOT:$PATH:$ANDROID_HOME/platform-tools
