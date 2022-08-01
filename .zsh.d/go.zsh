df73eec9404fb751519973ab85c9bc22e8fb2e8b
Kaoru Maeda <kaoru.maeda@dena.com>
2022-08-01 18:09:09 +0900

    goenv

--- .zsh.d/go.zsh
+++ .zsh.d/go.zsh
@@ -1,4 +1,12 @@
-export GO15VENDOREXPERIMENT=1
-export GOROOT=$(brew --prefix go)/libexec
-export GOPATH=$HOME/work
-export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+export GOENV_ROOT="$HOME/.goenv"
+export PATH="$GOENV_ROOT/bin:$PATH"
+if [ -x /opt/homebrew/bin/goenv ]; then
+  eval "$(/opt/homebrew/bin/goenv init -)"
+elif [ -x /usr/local/bin/goenv ]; then
+  eval "$(/usr/local/bin/goenv init -)"
+else
+  export GO15VENDOREXPERIMENT=1
+  export GOROOT=$(brew --prefix go)/libexec
+  export GOPATH=$HOME/work
+  export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+fi
